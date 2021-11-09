8897f312307b7b9f1959b66260bdf711409b4123
penrudee <61571633+penrudee@users.noreply.github.com>
2021-11-09 19:59:29 +0700

    Create delete_file_with_keyword.py

---
+++ delete_file_with_keyword.py
@@ -0,0 +1,10 @@
+
+pathE=r"E:/jpg"
+for root, dirs, files in os.walk(pathE):
+    for name in files:
+        for k in list_key_word:
+            if k in name:
+                f = os.path.join(pathE,name)
+                print(name)
+                os.remove(f)
+                
